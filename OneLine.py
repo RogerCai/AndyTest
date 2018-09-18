359257dab57db68524c9f57329385e03017f2420
houmt <houmingtao@evodecoration.com>
2018-09-18 09:51:03 +0800

    format the output

--- OneLine.py
+++ OneLine.py
@@ -1,3 +1,3 @@
 print('\n'.join([''.join([('TencentCloud'[(x-y)%12]if((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3<=0 else' ')for x in range(-30,30)])for y in range(15,-15,-1)]))
 
-print('\n\n\n         This is how an SW engineer to show love:)')
+print('\n          This is how an SW engineer to show love:)')
